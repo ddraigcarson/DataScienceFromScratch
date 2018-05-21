0703b0f2a5f803ec6d9b7e08816ddaef907a00c7
ddraigcarson <ddraigcarson@gmail.com>
2018-05-21 19:22:01 +0100

    Adding the decision tree

--- src/__init__.py
+++ src/__init__.py
@@ -1,94 +1,119 @@
-import csv
-import os.path
-import numpy as np
-import pandas as pd
-from bs4 import BeautifulSoup
-import requests
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-import seaborn as sns
+import math
+from collections import defaultdict, Counter
+from functools import partial
 
 
-pkmn_type_colors = ['#78C850',  # Grass
-                    '#F08030',  # Fire
-                    '#6890F0',  # Water
-                    '#A8B820',  # Bug
-                    '#A8A878',  # Normal
-                    '#A040A0',  # Poison
-                    '#F8D030',  # Electric
-                    '#E0C068',  # Ground
-                    '#EE99AC',  # Fairy
-                    '#C03028',  # Fighting
-                    '#F85888',  # Psychic
-                    '#B8A038',  # Rock
-                    '#705898',  # Ghost
-                    '#98D8D8',  # Ice
-                    '#7038F8',  # Dragon
-                    ]
+inputs = [
+    ({'level': 'Senior', 'lang': 'Java', 'tweets': 'no', 'phd': 'no'},    False),
+    ({'level': 'Senior', 'lang': 'Java', 'tweets': 'no', 'phd': 'yes'},   False),
+    ({'level': 'Mid', 'lang': 'Python', 'tweets': 'no', 'phd': 'no'},      True),
+    ({'level': 'Junior', 'lang': 'Python', 'tweets': 'no', 'phd': 'no'},   True),
+    ({'level': 'Junior', 'lang': 'R', 'tweets': 'yes', 'phd': 'no'},       True),
+    ({'level': 'Junior', 'lang': 'R', 'tweets': 'yes', 'phd': 'yes'},     False),
+    ({'level': 'Mid', 'lang': 'R', 'tweets': 'yes', 'phd': 'yes'},         True),
+    ({'level': 'Senior', 'lang': 'Python', 'tweets': 'no', 'phd': 'no'},  False),
+    ({'level': 'Senior', 'lang': 'R', 'tweets': 'yes', 'phd': 'no'},       True),
+    ({'level': 'Junior', 'lang': 'Python', 'tweets': 'yes', 'phd': 'no'},  True),
+    ({'level': 'Senior', 'lang': 'Python', 'tweets': 'yes', 'phd': 'yes'}, True),
+    ({'level': 'Mid', 'lang': 'Python', 'tweets': 'no', 'phd': 'yes'},     True),
+    ({'level': 'Mid', 'lang': 'Java', 'tweets': 'yes', 'phd': 'no'},       True),
+    ({'level': 'Junior', 'lang': 'Python', 'tweets': 'no', 'phd': 'yes'}, False)
+]
 
 
-def plot(df):
-    sns.countplot(x='Type1', data=df)
-    sns.lmplot(x='Attack', y='Defense', data=df, fit_reg=True)
-    plt.show()
+def entropy(class_probs):
+    return sum(-p * math.log(p, 2) for p in class_probs if p)
 
 
-def get_data_from_file(file):
-    df = None
-    if os.path.isfile(file):
-        print("Loading from file")
-        df = pd.read_csv(file)
-    else:
-        print("No local file")
-        return
-    return df
+def class_probabilities(labels):
+    total_count = len(labels)
+    return [count / total_count for count in Counter(labels).values()]
 
 
-def dedc(m, c, x, y):
-    return (2/len(x)) * (m * x + c - y)
+def data_entropy(labeled_data):
+    labels = [label for _, label in labeled_data]
+    probabilities = class_probabilities(labels)
+    return entropy(probabilities)
 
 
-def dedm(m, c, x, y):
-    return ((2 * x)/len(x)) * (m * x + c - y)
+def partition_entropy(subsets):
+    total_count = sum(len(subset) for subset in subsets)
+    return sum(data_entropy(subset) * len(subset)/total_count for subset in subsets)
 
 
-def sum_of_squared_error(m, c, x, y):
-    return np.sum(error(m, c, x, y)**2)/len(x)
+def partition_by(inputs, attribute):
+    groups = defaultdict(list)
+    for input in inputs:
+        key = input[0][attribute]
+        groups[key].append(input)
+    return groups
 
 
-def error(m, c, x, y):
-    return y - predict(m, c, x)
+def partition_entropy_by(inputs, attribute):
+    partitions = partition_by(inputs, attribute)
+    return partition_entropy(partitions.values())
 
 
-def predict(m, c, x):
-    return m * x + c
+def classify(tree, input):
+    if tree in [True, False]:
+        return tree
+
+    attribute, subtree_dict = tree
+
+    subtree_key = input.get(attribute)
+
+    if subtree_key not in subtree_dict:
+        subtree_key = None
+
+    subtree = subtree_dict[subtree_key]
+    return classify(subtree, input)
 
 
-def get_by_types(df, types):
-    return df[df.Type1.isin(types)]
+def build_tree_id3(inputs, split_candidates=None):
+    if split_candidates is None:
+        split_candidates = inputs[0][0].keys()
 
+    num_inputs = len(inputs)
+    num_trues = len([label for item, label in inputs if label])
+    num_falses = num_inputs - num_trues
 
-def linear_regression(df, x_column, y_column):
-    learning_step = 0.0001
-    m = 0
-    c = 0
-    n = 1000
-    print(df[y_column])
-    y = df[x_column].values     # numpy array
-    x = df[y_column].values    # numpy array
-    for i in range(n):
-        m = m - learning_step * np.sum(dedm(m, c, x, y))
-        c = c - learning_step * np.sum(dedc(m, c, x, y))
-        e = sum_of_squared_error(m, c, x, y)
-        print('e: {0}, m: {1}, c: {2} '.format(e, m, c))
+    if num_trues == 0:
+        return False
+    if num_falses == 0:
+        return False
+
+    if not split_candidates:
+        return num_trues >= num_falses
+
+    best_attribute = min(split_candidates, key=partial(partition_entropy_by, inputs))
+
+    partitions = partition_by(inputs, best_attribute)
+    new_candidates = [a for a in split_candidates if a != best_attribute]
+
+    subtrees = {attribute_value: build_tree_id3(subset, new_candidates) for attribute_value, subset in partitions.items()}
+
+    subtrees[None] = num_trues > num_falses
+    return best_attribute, subtrees
 
 
 def run():
-    # df = get_data_from_file('johto.csv')
-    # linear_regression(df, 'Attack', 'Defense')
-    df = get_data_from_file('data.csv')
-    linear_regression(df, 'x', 'y')
-    #plot(df)
+    tree = build_tree_id3(inputs)
+
+    o = classify(tree, {
+        "level": "Junior",
+        "lang": "Java",
+        "tweets": "yes",
+        "phd": "no"
+    })
+    print(o)
+
+    o2 = classify(tree, {
+        "level": "Junior",
+        "lang": "Java",
+        "tweets": "yes",
+        "phd": "yes"
+    })
+    print(o2)
 
 
 run()
